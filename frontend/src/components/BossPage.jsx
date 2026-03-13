db73584bd1c102835306a6e2813cf9d3fa5ba18d
saint sinner <alexlloyd221999@gmail.com>
2026-03-13 14:04:38 +0300

    set

--- frontend/src/components/BossPage.jsx
+++ frontend/src/components/BossPage.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/BossPage.module.css";
 //import CorleoneBg from "../../public/Corleone.mp4";
-//import CorleoneBg2 from "@/assets/background";
+import CorleoneBg2 from "@/assets/background";
 import { useEffect, useRef } from "react";
 import { canvasLightning } from "../canvas.js";
 //import tg from "@/assets/telegram";
@@ -49,7 +49,7 @@ export default function BossPage(){
                 muted
                 className={styles.videoBg2}
             >
-                <source src="@/assets/background.mp4" type="video/mp4"/>
+                <source srcSet={CorleoneBg2} type="video/mp4"/>
             </video>
             {/*<canvas ref={canvasRef} className={styles.canvas}/>*/}
         </div>
