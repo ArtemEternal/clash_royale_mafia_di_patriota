6dfd620a0cbd24d0bc0ea6b9db49b048d49000fe
saint sinner <alexlloyd221999@gmail.com>
2026-03-13 14:16:41 +0300

    test

--- frontend/src/components/BossPage.jsx
+++ frontend/src/components/BossPage.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/BossPage.module.css";
 //import CorleoneBg from "../../public/Corleone.mp4";
 //import CorleoneBg2 from "@/assets/background";
+import bg_desktop from "../../public/test_bg.jpg";
 import { useEffect, useRef } from "react";
 import { canvasLightning } from "../canvas.js";
 //import tg from "@/assets/telegram";
