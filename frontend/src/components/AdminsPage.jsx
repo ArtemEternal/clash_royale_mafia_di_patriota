18ab4b06609479466c939aab7dc1cd0d325a0a83
saint sinner <alexlloyd221999@gmail.com>
2026-03-13 23:00:54 +0300

    test 24.5

--- frontend/src/components/AdminsPage.jsx
+++ frontend/src/components/AdminsPage.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/AdminsPage.module.css";
 import GoBackButton from "./GoHomeButton.jsx";
-//import hui from "../../public/test_bg.jpg";
+import hui from "../../public/miner.jpg";
 
 const members = [
     {nickname: "Михал Палыч", role: "Дон Vertical ll", tg: "Секретно", photo: "../../public/mihalpalich.png"},
@@ -37,7 +37,8 @@ export default function AdminsPage(){
             {members.map(member => (
                 <div className={styles.memberCard} key={member.nickname}>
                     <div className={styles.memberPhotoContainer}>
-                        <img srcSet={member.photo}/>
+                        {/*<img srcSet={member.photo}/>*/}
+                        <img srcSet={hui}/>
                     </div>
                     <div className={styles.memberAboutContainer}>
                         <h4>{member.nickname}</h4>
