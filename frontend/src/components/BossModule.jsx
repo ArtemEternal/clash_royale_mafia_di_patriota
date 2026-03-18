cd187d0a8741cdd1920236e46c35ca153806f368
saint sinner <alexlloyd221999@gmail.com>
2026-03-18 13:46:06 +0300

    boss page fix 4

---
+++ frontend/src/components/BossModule.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/BossModule.module.css";
+
+export default function BossModule(){
+
+    return<>
+        <div className={styles.contentContainer}>
+
+        </div>
+    </>
+}
